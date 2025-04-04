dcf8769c4ad66c0b665ec5c4e03b8afffa1e07b4
Chris2810 <christophercd960@gmail.com>
2025-04-03 19:36:43 -0600

    Login fixed

---
+++ Build_Xpert/Model/RegisterUser.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Build_Xpert.Model
+{
+    public class RegisterUser
+    {
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        public string UserName { get; set; } = "";
+
+        [Required]
+        [Phone]
+        [Display(Name = "Teléfono")]
+        public string PhoneNumber { get; set; } = "";
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = "";
+
+        [Required]
+        [StringLength(100, ErrorMessage = "La contraseña debe tener entre {2} y {1} caracteres.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; } = "";
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string ConfirmPassword { get; set; } = "";
+    }
+}
